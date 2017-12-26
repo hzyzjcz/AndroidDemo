bd8d038de8eec8cba0e359fd91636ac64cf0406c
app_zhouxiaofeng <xiaofeng_code@163.com>
2017-12-26 11:24:48 +0800

    删除

--- app/src/main/java/com/zhouxiaofeng/demo/TestService.java
+++ app/src/main/java/com/zhouxiaofeng/demo/TestService.java
@@ -1,13 +1,10 @@
 package com.zhouxiaofeng.demo;
 
-import android.app.Notification;
-import android.app.PendingIntent;
+
 import android.app.Service;
 import android.content.Intent;
-import android.icu.text.UnicodeSetSpanner;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
