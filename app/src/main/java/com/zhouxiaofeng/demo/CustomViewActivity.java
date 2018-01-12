3b04ca7c1ba366a9a4d55ef54d6c07e4d804aac5
app_zhouxiaofeng <xiaofeng_code@163.com>
2018-01-12 09:40:18 +0800

    123

--- app/src/main/java/com/zhouxiaofeng/demo/CustomViewActivity.java
+++ app/src/main/java/com/zhouxiaofeng/demo/CustomViewActivity.java
@@ -3,6 +3,7 @@ package com.zhouxiaofeng.demo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 /**
  * Created by Administrator on 2018/1/4.
@@ -13,5 +14,10 @@ public class CustomViewActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);
     }
 }
